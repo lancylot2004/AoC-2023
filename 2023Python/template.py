b17d7c089535e1336616a87be46f3356adbd57d0
Lancelot Liu <lancylot2004@proton.me>
2023-12-22 13:33:12 +0300

    Better template using decorators.

--- 2023Python/template.py
+++ 2023Python/template.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from functools import wraps
 from os import path
 from time import perf_counter_ns
 from tqdm import tqdm
@@ -16,7 +17,114 @@ def getLines(day: int) -> Generator[str, None, None]:
     with open(path.join(path.dirname(__file__), "inputs", f"{day}.txt"), 'r') as file:
         for line in file:
             yield line
-            
+
+def getFile(day: int) -> str:
+    """Gets the contents of the input file for the given day.
+
+    Args:
+        day (int): Day to get the input for.
+
+    Returns:
+        str: Contents of the input file.
+    """
+    with open(path.join(path.dirname(__file__), "inputs", f"{day}.txt"), 'r') as file:
+        return file.read()
+    
+def time(name: str) -> Callable[..., Any]:
+    """Decorator that times a function's execution and prints the time alongw its result.
+
+    Args:
+        method (Callable[..., T]): Function to be timed and evaluated.
+
+    Returns:
+        Callable[..., T]: Decorated function.
+    """
+
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        """Decorator function to time the provided function.
+
+        Args:
+            func (Callable[..., Any]): Function to time.
+
+        Returns:
+            Callable[..., Any]: Decorated function to time and print its execution.
+        """
+
+        @wraps(func)
+        def wrapper(*args: Any) -> Any:
+            """Wrapped function to time and print its execution.
+
+            Args:
+                *args (Any): Arguments to pass to the function.
+
+            Returns:
+                Any: Result of the function call.
+            """
+
+            start = perf_counter_ns()
+            res = func(*args)
+            end = perf_counter_ns()
+            print(f"{name} after {(end - start) / 1_000_000:.3f}ms: {res}")
+            return res
+
+        return wrapper
+
+    return decorator
+
+def timeAvg(name: str, repeats: int) -> Callable[..., Any]:
+    """Decorator that times a function's execution and prints the average time along with its result.
+
+    Args:
+        name (str): Human-readable name of the function call.
+        repeats (int): Number of times to call the function.
+
+    Returns:
+        Callable[..., Any]: Decorated function to time and print its execution.
+    """
+
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        """Decorator function to time the provided function.
+
+        Args:
+            func (Callable[..., Any]): Function to time.
+
+        Returns:
+            Callable[..., Any]: Decorated function to time and print its execution.
+        """
+
+        @wraps(func)
+        def wrapper(*args: Any) -> Any:
+            """Wrapped function to time and print its execution.
+
+            Args:
+                *args (Any): Arguments to pass to the function.
+
+            Returns:
+                Any: Result of the function call.
+            """
+            assert repeats > 0
+
+            times = []
+            res = None
+            for _ in tqdm(range(repeats), desc = name, leave = False):
+                start = perf_counter_ns()
+                # Deepcopy args since some functions might modify them.
+                tmpRes = func(*deepcopy(args))
+                end = perf_counter_ns()
+                times.append(end - start)
+
+                if res is not None:
+                    assert tmpRes == res
+                res = tmpRes
+
+            avgTime = sum(times) / len(times)
+            print(f"{name} after avg. {avgTime / 1_000_000:.3f}ms: {res}")
+            return res
+
+        return wrapper
+
+    return decorator
+
 def timeAndPrint(name: str, fun: Callable[..., Any], *args: Any) -> None:
     """Calls `fun` and prints the time it took to run, along with its result.
 
